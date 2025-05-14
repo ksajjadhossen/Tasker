9183b2ca377a5a2461f22e18f7594d175f800e8b
Khandaker Sajjad Hossen <ksajjadhossen07@gmail.com>
2025-05-15 02:08:15 +0600

    Hero section and footer successfully created.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
+import HeroSection from "./components/HeroSection/HeroSection";
+import Footer from "./shared/Footer/Footer";
 import Navbar from "./shared/Navbar/Navbar";
 
 function App() {
   return (
     <>
       <Navbar></Navbar>
+      <HeroSection></HeroSection>
+      <Footer></Footer>
     </>
   );
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,12 @@
+import Footer from "../../shared/Footer/Footer";
 import Navbar from "../../shared/Navbar/Navbar";
 
 export default function Home() {
   return (
     <>
       <Navbar></Navbar>
+
+      <Footer></Footer>
     </>
   );
 }
